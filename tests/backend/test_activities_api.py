3e92f2bd9e0b9749465eb3cdc3387b633c2018f6
WALLAM <wallamfdossantos@gmail.com>
2026-06-11 02:52:38 +0000

    ex4 v3 - fix test activities

--- tests/backend/test_activities_api.py
+++ tests/backend/test_activities_api.py
@@ -13,7 +13,7 @@ def test_get_activities_returns_activity_map(client):
 
 
 def test_root_redirects_to_static_index(client):
-    response = client.get("/", allow_redirects=False)
+    response = client.get("/", follow_redirects=False)
 
     assert response.status_code == 307
     assert response.headers["location"] == "/static/index.html"
